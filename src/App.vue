b2555b9b1f1df86da46f22b458a828e2a0482fca
Ilya Erokhin <sniffsla@gmail.com>
2018-01-10 17:27:02 +0300

    App.vue - true singlefile

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
-<template src="./App.html"></template>
-<style scoped src="./App.css"></style>
-<script lang="ts" src="./App.ts"></script>
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component
+  export default class App extends Vue {
+
+  }
+</script>
